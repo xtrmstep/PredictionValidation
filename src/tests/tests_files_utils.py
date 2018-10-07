c9269c30e75fc32ac6068dbe23ea59e166125a75
Alexander Goida <alexander.goida@gmail.com>
2018-10-07 15:26:00 +0300

    Added PyCharm project + file utilities

---
+++ src/tests/tests_files_utils.py
@@ -0,0 +1,6 @@
+import files_utils as fu
+
+# this folder will be cleaned so make sure you are not deleting important files
+clearning_folder = '../../data/processed/'
+
+fu.clear_folder(clearning_folder)
