1061fec23c55be7249da2b5840a7ce82ba7cbc66
Alexander Goida <alexander.goida@gmail.com>
2018-10-23 11:12:30 +0300

    Fixed grouping by location and plu
    Removing zero qty

--- src/transform_data.py
+++ src/transform_data.py
@@ -27,16 +27,16 @@ class DataTransformationProcessor:
 
     def __init__(self, source_files, destination_folder):
         self._transformation_pipelines = [
-            self._pipeline_extract_locations,
+            self._pipeline_extract_groups,
             self._pipeline_extract_cities
         ]
         self._source_files = source_files
         self._destination_folder = destination_folder
 
-    def _pipeline_extract_locations(self, customer_name, customer_data):
+    def _pipeline_extract_groups(self, customer_name, customer_data):
         """ task can return several transformations, each of transformed data should have a distinct file name """
 
-        data_blocks = self._default_pipeline_task_designate_chunks(customer_name, customer_data)
+        data_blocks = self._task_designate_by_location_plu(customer_name, customer_data)
         # other tasks go here...
         data_blocks = self._remove_zero_quantity(data_blocks)
         data_blocks = self._add_datetime_parts(data_blocks)
@@ -56,15 +56,15 @@ class DataTransformationProcessor:
         cities = pd.DataFrame({'City': cities})
         return [(file, cities)]
 
-    def _default_pipeline_task_designate_chunks(self, customer_name, customer_data):
+    def _task_designate_by_location_plu(self, customer_name, customer_data):
         """ designate data to chunks and files to store them on disk """
         # get script's folder to build an absolute path to a destination folder
         # absolute path is required for pandas.dataFrame to save data
         script_dir = os.path.dirname(os.path.abspath(__file__))
-        grouped = customer_data.groupby(['LocationID'])
+        grouped = customer_data.groupby(['LocationID','PLU'])
         result = []
         for name, group in grouped:
-            file = os.path.join(script_dir, self._destination_folder, '{}_location_{}.csv'.format(customer_name, name))
+            file = os.path.join(script_dir, self._destination_folder, '{}_(location,plu)_{}.csv'.format(customer_name, name))
             result += [(file, group)]
         return result
 
@@ -80,8 +80,9 @@ class DataTransformationProcessor:
     def _remove_zero_quantity(data_blocks):
         result = []
         for file, data in data_blocks:
-            data = data[data['Quantity'] > 0]
-            result += [(file, data)]
+            data = data[data['Quantity'] != 0]
+            if len(data) > 0:
+                result += [(file, data)]
 
         return result
 
